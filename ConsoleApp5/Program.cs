628ab5bc9c6b2a96e25a0d08cfae99793124f50e
The4yo <eugshpak@yandex.ru>
2023-04-16 22:16:51 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+Console.WriteLine("Введите фамилию человек, чей адрес почты вы хотите получить!");
+string fam = Console.ReadLine();
+Hashtable email = new Hashtable()
+{
+    ["Крючков"] = "janekruchkov@bk.ru",
+    ["Манера"] = "maneraXxX@manera.int",
+    ["Бочаров"] = "usatayabubna@srya.com",
+    ["Афонин"] = "afoninje@morph.leave"
+};
+
+if (email.ContainsKey(fam))
+{
+    Console.WriteLine($"Адрес почты {email[fam]}");
+}
+
+else
+{
+    Console.WriteLine("Такого человека не существует!");
+}
